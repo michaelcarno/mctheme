1e8ecb8fc4fe1955e96b4f61845232b1b6ade8b8
Michael Carno <michaelcarno@mail.ru>
2023-07-12 01:00:03 +0700

    initial comit

---
+++ lua/mctheme/lib/config.lua
@@ -0,0 +1,24 @@
+M = {}
+
+M.default = {
+  palette = "mctheme",
+  termguicolors = true,
+  terminal_colors = true,
+  background = "dark",
+  palettes = {
+    global = {},
+    astrodark = {},
+    astrolight = {},
+  },
+  highlights = {
+    global = {},
+    astrodark = {},
+    astrolight = {},
+  },
+  plugin_default = "auto",
+  plugins = {},
+}
+
+function M.user_config(opts) return vim.tbl_deep_extend("force", M.default, opts or {}) end
+
+return M
