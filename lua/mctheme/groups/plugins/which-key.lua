b369486c4ec729995c5b651815616c25332538f3
Michael Carno <michaelcarno@mail.ru>
2023-08-03 05:22:01 +0700

    Added more colors to whichkey plugin

--- lua/mctheme/groups/plugins/which-key.lua
+++ lua/mctheme/groups/plugins/which-key.lua
@@ -1,5 +1,10 @@
 local function callback()
   return {
+
+    WhichKey = { fg = C.fg },
+    WhichKeyBorder = { fg = C.none },
+    WhichKeySeperator = { fg = C.white_1 },
+    WhichKeyValue = { fg = C.white_2 },
     WhichKeyFloat = { fg = C.fg },
     WhichKeyDesc = { fg = C.purple },
     WhichKeyGroup = { fg = C.blue },
