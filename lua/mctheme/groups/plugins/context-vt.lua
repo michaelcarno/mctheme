d62513ad7b0bf50fa065d1fd6cb570ee5dad8ca0
Michael Carno <bondarenko87@gmail.com>
2024-05-30 21:59:26 +0700

    Changed: context-vt fg color to same as Comment

--- lua/mctheme/groups/plugins/context-vt.lua
+++ lua/mctheme/groups/plugins/context-vt.lua
@@ -1,8 +1,8 @@
 local function callback()
   return {
-    CustomContextVt = { fg = C.closeTagContext, bg = C.closeTagContextBg, italic = false },
+    -- CustomContextVt = { fg = C.subtext1, bg = C.closeTagContextBg, italic = false },
     -- CustomContextVt = { fg = C.subtext1, bg = C.none, italic = true },
-    -- CustomContextVt = {link = "Comment"},
+    CustomContextVt = {link = "Comment"},
   }
 end
 
