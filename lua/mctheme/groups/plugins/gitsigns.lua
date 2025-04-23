ea56327518c45d4259c969f005059115e9c34ac8
michaelcarno <bondarenko87@gmail.com>
2025-04-23 15:20:17 +0700

    changes: styles

--- lua/mctheme/groups/plugins/gitsigns.lua
+++ lua/mctheme/groups/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ local function callback()
   return {
     GitSignsAdd = { fg = C.green, bg = C.none },
     GitSignsChange = { fg = C.dark_orange, bg = C.none },
-    GitSignsDelete = { fg = C.dark_red, bg = C.none },
+    GitSignsDelete = { fg = C.red_bright, bg = C.none },
     MoreMsg = { fg = C.green, bold = true },
     ModeMsg = { fg = C.subtext0, bold = true },
   }
